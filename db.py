d6626fd4e445df63a22e97633efb9c80c0c2df61
financial-Fraud-detection <kiran>
2026-05-02 17:50:24 +0530

    environment change

--- db.py
+++ db.py
@@ -3,9 +3,9 @@ import os
 
 def get_db():
     return mysql.connector.connect(
-        host=os.environ.get("DB_HOST", "localhost"),
-        user=os.environ.get("DB_USER", "root"),
-        password=os.environ.get("DB_PASSWORD", "UDAYsql123"),
-        database=os.environ.get("DB_NAME", "task_manager"),
-        port=os.environ.get("DB_PORT", 3306)
+        host=os.environ.get("MYSQLHOST", "localhost"),
+        user=os.environ.get("MYSQLUSER", "root"),
+        password=os.environ.get("MYSQLPASSWORD", "UDAYsql123"),
+        database=os.environ.get("MYSQLDATABASE", "task_manager"),
+        port=int(os.environ.get("MYSQLPORT", 3306))
     )
